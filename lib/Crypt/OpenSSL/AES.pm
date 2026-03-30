cc4960313d853028248512229fde0a004a18a333
Timothy Legge <timlegge@gmail.com>
2026-03-29 22:20:22 -0300

    We do not export anything

--- lib/Crypt/OpenSSL/AES.pm
+++ lib/Crypt/OpenSSL/AES.pm
@@ -16,23 +16,6 @@ require Exporter;
 
 our @ISA = qw(Exporter);
 
-# Items to export into callers namespace by default. Note: do not export
-# names by default without a very good reason. Use EXPORT_OK instead.
-# Do not simply export all your public functions/methods/constants.
-
-# This allows declaration	use Crypt::OpenSSL::AES ':all';
-# If you do not need this, moving things directly into @EXPORT or @EXPORT_OK
-# will save memory.
-our %EXPORT_TAGS = ( 'all' => [ qw(
-	
-) ] );
-
-our @EXPORT_OK = ( @{ $EXPORT_TAGS{'all'} } );
-
-our @EXPORT = qw(
-	
-);
-
 require XSLoader;
 XSLoader::load('Crypt::OpenSSL::AES', $VERSION);
 
